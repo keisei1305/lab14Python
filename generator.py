55c0a25e29aeabe49dda6f6eddc096774eb14efb
keisei1305 <kippop88@gmail.com>
2024-02-06 15:41:13 +0300

    Дописал все генерации для БД

--- generator.py
+++ generator.py
@@ -12,10 +12,12 @@ class Generator:
     names = ["Евгений", "Егор", "Сергей", "Иван", "Начын", "Кирилл", "Михаил", "Самба"]
     lastnames = ["Татарян", "Парфинцов", "Минкеев", "Петроченко", "Биче-Оол", "Гурин", "Ховенмей", "Ховалыг"]
     genders = ["М", "Ж", "Хеликоптер"]
-    customer_status = ["Заблокирован", "Постоянный клиент", "Клиент", "Важная персона"]
+    customer_statuses = ["Заблокирован", "Постоянный клиент", "Клиент", "Важная персона"]
     work_types = ["Фотография", "Видеография"]
     work_statuses = ["Выполнено", "В работе", "Отложено", "Запланировано", "Заблокировано"]
     media_types = ["Фото", "Видео", "Аудио"]
+    album_names = ["Лето", "Зима", "Выпускной", "День рождение"]
+    cools = ["Круто", "Весело", "Стыдно", "Грустно", "Вот это да"]
 
     @staticmethod
     def take_random_seed():
@@ -39,7 +41,7 @@ class Generator:
     @staticmethod
     @get_random
     def __get_random_customer_status():
-        return Generator.customer_status
+        return Generator.customer_statuses
 
     @staticmethod
     @get_random
@@ -57,6 +59,18 @@ class Generator:
         return Generator.work_types
 
     @staticmethod
+    def __generate_date():
+        year = str(random.randint(2020, 2024))
+        month = random.randint(1, 12)
+        day = random.randint(1, 31)
+        if month < 10:
+            month = '0'.join(str(month))
+        if day < 10:
+            day = '0'.join(str(day))
+        dates = [year, '-', month, '-', day]
+        return ''.join(dates)
+
+    @staticmethod
     def __generate_number_phone():
         numbers = [8, 9]
         for i in range(9):
@@ -65,27 +79,46 @@ class Generator:
         return ''.join(map(str, numbers))
 
     @staticmethod
+    def __generate_album_name():
+        strings = [random.choice(Generator.album_names), str(random.randint(2000, 2024)), random.choice(Generator.cools)]
+        return ''.join(strings)
+
+    @staticmethod
     def generate_human():
         return (Generator.__get_random_name(), Generator.__get_random_lastname(), Generator.__generate_number_phone(),
                 Generator.__get_random_gender())
 
     @staticmethod
     def generate_customer():
-        return *Generator.generate_human(), Generator.__get_random_customer_status
+        return *(Generator.generate_human()), Generator.__get_random_customer_status()
 
     @staticmethod
     def generate_executor():
         return Generator.generate_human()
 
     @staticmethod
-    def generate_order(customer_id):
-        year = str(random.randint(2020, 2024))
-        month = random.randint(1, 12)
-        day = random.randint(1 ,31)
-        if month < 10:
-            month = '0'.join(str(month))
-        if day < 10:
-            day = '0'.join(str(day))
-        dates = [year, '-', month, '-', day]
-        return ''.join(dates)
+    def generate_order(customer_id, report_id=None):
+        return customer_id, Generator.__generate_date(), random.randint(500, 3500), report_id
 
+    @staticmethod
+    def generate_album(size=0):
+        return Generator.__generate_album_name(), size
+
+    @staticmethod
+    def generate_report(album_id=None):
+        return Generator.__get_random_work_status, album_id
+
+    @staticmethod
+    def generate_media(size=0, album_id=None):
+        if size == 0:
+            size=random.randint(1, 100)
+        return (Generator.__get_random_media_type, size,
+                Generator.__generate_album_name().join(str(random.randint(1, 100))), album_id)
+
+    @staticmethod
+    def generate_sessions(command_id, order_id):
+        return command_id, random.randint(1,2), order_id
+
+    @staticmethod
+    def generate_command(mas_of_executors):
+        return random.choices(mas_of_executors, k=random.randint(1, 4))
